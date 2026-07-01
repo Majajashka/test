1e9437da0c2ae90267c7d313e681bb8582359d8d
aleksi55 <aleksi.berianidze.1@btu.edu.ge>
2026-07-01 08:20:42 +0400

    Delete app/database/schema_01_languages.sql

--- app/database/schema_01_languages.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE languages (
-    code        TEXT PRIMARY KEY,
-    name_native TEXT NOT NULL,
-    name_en     TEXT NOT NULL,
-    is_active   INTEGER NOT NULL DEFAULT 1 CHECK (is_active IN (0, 1))
-);
